7ce39db756ee0daca5a81f748ae25eb8d16a2703
Ayushikanoje <ayushikanoje310@gmail.com>
2025-06-26 21:00:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict";// treat all js code as newer version
+//alert(3+3)//(not use here available hei but console m run ho jata h) we are using nodejs , not browser
+ 
+ 
+ console.log(3+
+    3
+ );// code readability should be high
+
+ console.log("AYUSHI");
+
+ let name= "ayushi"
+let age= 21
+let isLoggedIn= false
+
+//number=> 2 to power 53
+// bigint=>large number
+//string=>""
+//boolean=>true/false
+// null => stadalone value  (null ek object hei (type))
+//undefined=>value not assign
+//symbol=>uniqe
+
+
+//object
+
+console.log(typeof "ayushi");
+
+ 
+ 
