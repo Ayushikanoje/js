795510049cc0b7e3bc9677f5488560c000784a8d
Ayushikanoje <ayushikanoje310@gmail.com>
2025-06-27 21:19:07 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+ let value=3
+ let negValue=-value
+ //console.log(negValue);
+ 
+//  console.log(2+3);
+//  console.log(2-3);
+//  console.log(2/3);
+//  console.log(2*3);
+//  console.log(2**3);//power
+//  console.log(2%3);
+
+let str1="hello"
+let str2=" ayushi"
+ 
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1" + 2 );
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);//conversion hoga not preffered
+console.log(+"");
+
+let num1,num2 ,num3
+
+num1=num2=num3=2+3
+//  not preffered
+
+
