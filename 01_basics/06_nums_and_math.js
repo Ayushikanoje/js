158615e8e5c80a12e80bfcfb642fb68a6731c5a6
Ayushikanoje <ayushikanoje310@gmail.com>
2025-07-01 12:20:31 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+ const score= 200
+ console.log(score);
+ 
+
+ //agr hum chahatein h ki jo type h wo number hi ho to aise define kr sktein h
+ const balance= new Number(100);
+ console.log(balance);
+
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(2));
+ 
+ 
+ const otherNumber =124.8966
+ console.log(otherNumber.toPrecision(3));
+ 
+ const hundreds=1000000
+ console.log(hundreds.toLocaleString('en-IN'));
+ 
+ /////////////////////////MATHS////////////////////
+
+ 
