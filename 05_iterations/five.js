5de2da8752dcd39eb7a0f57593e63d7dc5dcabf9
Ayushikanoje <ayushikanoje310@gmail.com>
2025-07-11 17:55:46 +0000

    loops filter

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+ //const coding = [ "js","ruby","java","python","cpp"]
+
+
+//  const value= coding.forEach((item)=>{
+//    // console.log(item);
+//     return item;
+//     //for each does'nt return anything
+//  })
+
+//  console.log(value);
+ 
+
+////// filter return the value where as for each does'nt
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+// const newNumbs=myNums.filter((num)=>{
+//     return num>4 // agr curly paranthesis lagaye hei toh return likhna hi padega aur round paranthesis lagaye toh usme likhne ki jarurat nhi h
+// })
+
+
+//console.log(newNumbs);
+
+
+
+
+// const newNums=[]
+// myNums.forEach((num)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
+
+
+
+
